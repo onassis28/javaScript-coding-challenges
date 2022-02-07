cd206834990634ae441e60ee27c9efa6d0094979
Onassis <onassis.sowah@gmail.com>
2022-02-07 21:00:43 +0100

    created the reverseArray function

---
+++ 1. reverse array.js
@@ -0,0 +1,14 @@
+
+// create an arrow function, iterate and push the reverse into a new array
+const theReversedArray = array => {
+    let theReversed = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversed.push(array[i]);
+    }
+    return theReversed 
+}
+
+// create an array sample and call the reversed arrray function
+const sample = ['local', 'ghana', 'germany', 'norway']
+
+console.log(theReversedArray(sample))
