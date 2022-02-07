2349f77cbce6df1fb25fc1266bad1fe68d1bea3d
Onassis <onassis.sowah@gmail.com>
2022-02-07 21:14:27 +0100

    created a greeting function

---
+++ 2. greetings.js
@@ -0,0 +1,11 @@
+const greet = array => {
+    for (let i = 0; i < array.length; i++) {
+          console.log('You are Welcome ' + array[i] 
+           );
+    }
+}
+
+
+const names = ["Onassis", "Sowah", "Junior", "Kwame"];
+
+greet(names)
